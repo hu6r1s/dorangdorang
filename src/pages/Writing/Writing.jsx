f91470603ab5b8df8a8c20f7aa941d4ac3a888ad
hu6r1s <darmaa333@gmail.com>
2023-09-21 17:42:35 +0900

    [feat] board

--- src/pages/Writing/Writing.jsx
+++ src/pages/Writing/Writing.jsx
@@ -1,6 +1,11 @@
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import axios from "axios";
 import Header from "components/Header";
-import MuiSelect from "components/MuiSelect";
-import React, { useState } from "react";
+import { useState } from "react";
 import {
   FlexBox,
   WritingContainer,
@@ -23,18 +28,68 @@ const Writing = () => {
       ...formData,
       [name]: value,
     });
-    console.log(formData);
   };
 
-  
+  const eventWriting = async (e) => {
+    e.preventDefault();
+    try {
+      if (formData.category === "gomin") {
+        await axios.post(
+          `${process.env.REACT_APP_SERVER_API}/dorandoran/create`,
+          null,
+          {
+            params: {
+              title: formData.title,
+              description: formData.contents,
+              userId: "userId"
+            }
+          }
+        )
+      } else {
+        await axios.post(
+          `${process.env.REACT_APP_SERVER_API}/event/create`,
+          null,
+          {
+            params: {
+              title: formData,
+              description: formData.contents,
+              category: formData.category,
+              status: ""
+            }
+          }
+        )
+
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
   return (
     <>
       <Header />
-      <form>
+      <form onSubmit={eventWriting}>
         <WritingContainer>
           <FlexBox>
             <WritingTitleInput name="title" value={formData.title} onChange={handleChange} placeholder="제목" required />
-            <MuiSelect />
+            <Box sx={{ minWidth: 120, width: '140px', height: '70px', marginLeft: '30px' }}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">카테고리</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={formData.category}
+                  label="카테고리"
+                  onChange={handleChange}
+                  name="category"
+                >
+                  <MenuItem value="saecham">새참 먹자</MenuItem>
+                  <MenuItem value="poom">품앗이</MenuItem>
+                  <MenuItem value="gomin">고민 토론</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
           </FlexBox>
           <WritingTextArea name="contents" value={formData.contents} onChange={handleChange} placeholder="내용" required />
           <WritingSubmitBtton>글 작성</WritingSubmitBtton>
